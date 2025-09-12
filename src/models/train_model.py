0a4bc3af651300794ad4d3e8ad0d036d5cc1cb67
Sajid Islam <sajnyc360@gmail.com>
2025-09-11 22:53:03 -0400

    Added random forest model, ridge model, and histgradientboosting model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,8 +1,36 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor
 import numpy as np
+import matplotlib.pyplot as plt
+
+def train_linear(X_train, y_train):
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def train_random_forest(X_train, y_train):
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+    return model
+
+def train_ridge(X_train, y_train):
+    model = Ridge()
+    model.fit(X_train, y_train)
+    return model
+
+def train_hist_gb(X_train, y_train):
+    model = HistGradientBoostingRegressor()
+    model.fit(X_train, y_train)
+    return model
+
+def evaluate_data(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    print("R2 score: ", r2_score(y_test, y_pred))
+    print ("RMSE: ", np.sqrt(mean_squared_error(y_test, y_pred)))
+    return y_pred
 
 df = pd.read_csv("data/qb_passing_2024.csv")
 
@@ -16,10 +44,23 @@ y = top_players["Yds"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+#model = train_linear(X_train, y_train)
+#model = train_random_forest(X_train, y_train)
+model = train_ridge(X_train, y_train)
+#model = train_hist_gb(X_train, y_train)
 
-y_pred = model.predict(X_test)
+evaluate_data(model, X_test, y_test)
 
-print("R2 score: ", r2_score(y_test, y_pred))
-print ("RMSE: ", np.sqrt(mean_squared_error(y_test, y_pred)))
+
+
+
+
+# model = LinearRegression()
+# model.fit(X_train, y_train)
+
+# residuals = y_test - y_pred
+# plt.scatter(y_pred, residuals)
+# plt.axhline(0, color='red')
+# plt.xlabel("Predicted yards")
+# plt.ylabel("Residuals")
+# plt.show()
